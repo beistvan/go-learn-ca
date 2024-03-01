7aa4196d8a2815a8a3b8a6d4381e688276bfbc58
Beregszászi István <beistvan@gmail.com>
2024-03-01 22:19:58 +0100

    Hello World Go And It's Build And Run

---
+++ hello-world-go-and-its-build.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// If the file name is greet.go
+/*
+Compile:
+go build greet.go
+
+Run:
+./greet
+go run greet.go
+
+*/
